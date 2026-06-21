9aadb51365f02eec037aa973440cafeefff9b8db
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-06-21 08:09:02 +0800

    files upload

---
+++ code/alien.py
@@ -0,0 +1,36 @@
+import pygame, os
+
+# Get the directory of this script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
+
+class Alien(pygame.sprite.Sprite):
+	def __init__(self,color,x,y):
+		super().__init__()
+		file_path = os.path.join(parent_dir, 'graphics', color + '.png')
+		self.image = pygame.image.load(file_path).convert_alpha()
+		self.rect = self.image.get_rect(topleft = (x,y))
+
+		if color == 'red': self.value = 100
+		elif color == 'green': self.value = 200
+		else: self.value = 300
+
+	def update(self,direction):
+		self.rect.x += direction
+
+class Extra(pygame.sprite.Sprite):
+	def __init__(self,side,screen_width):
+		super().__init__()
+		self.image = pygame.image.load(os.path.join(parent_dir, 'graphics', 'extra.png')).convert_alpha()
+		
+		if side == 'right':
+			x = screen_width + 50
+			self.speed = - 3
+		else:
+			x = -50
+			self.speed = 3
+
+		self.rect = self.image.get_rect(topleft = (x,80))
+
+	def update(self):
+		self.rect.x += self.speed
